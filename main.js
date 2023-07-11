5510d3ca0f1caa9b30f27f2736701ffab9f762f1
gtpman04 <gtpman04@yahoo.com>
2023-07-10 20:46:52 -0500

    moved folders

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
-let btn=document.getElementById('btn');
+window.onclick = makeBtn;
+let btn = document.getElementById('btn');
 
-btn.onclick = function() {
-alert('I am pressed for time!');
+btn.onclick = function () {
+    alert('I am pressed for time!');
 
-changeText();
+    changeText();
 }
 
+function changeText() {
+    btn.innerText = "that tickles";
 
-function changeText(){
-btn.innerText="that tickles";
-
-changeStyle();
-changeBorder();
-
+    changeStyle();
+    changeBorder();
+    makeBtn();
 }
 
-function changeStyle(){
-
-    btn.style.backgroundColor="green";
-
+function changeStyle() {
+    btn.style.backgroundColor = "green"; 
 }
 
-function changeBorder(){
-
-    btn.style.borderColor="yellow";
+function changeBorder() {
+    btn.style.borderColor = "yellow";
 }
+
+function makeBtn() {
+    let element = document.createElement("button");
+    element.innerText = "Hello World";
+    element.classList.add("btnbtn");
+    // element.addEventListener("click", colorChange);
+    document.getElementById('srvbtn').append(element);
+} 
+
+
+
 
 const menu = document.getElementById("menu");
 
 Array.from(document.getElementsByClassName("menu-item"))
-.forEach((item, index) => {
-    item.onmouseover = () => {
-        menu.dataset.activeIndex = index;
-
-    }
-
-})
+    .forEach((item, index) => {
+        item.onmouseover = () => {
+            menu.dataset.activeIndex = index;
+        }
+    })   
